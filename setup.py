af948e0924e7ccd05b6c96938a2a38827b7b5123
channudamrayatwing <channudam.ray@wingbank.com>
2024-06-04 10:20:05 +0700

    update release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.1"
+__version__ = "1.0.0.0"
 REPO_NAME = "khmerocr"
 PKG_NAME= "khmerocr"
 AUTHOR_USER_NAME = "channudamatwing"
